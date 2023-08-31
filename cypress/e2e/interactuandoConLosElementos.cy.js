8a8297da1ca6123df384bb817f3428436ecefadc
Ivan Maldonado <IMaldonado2@teco.com.ar>
2023-08-31 12:21:59 -0300

    Cambios hasta el punto 16

--- cypress/e2e/interactuandoConLosElementos.cy.js
+++ cypress/e2e/interactuandoConLosElementos.cy.js
@@ -24,13 +24,35 @@ describe( 'Interactuar con los elementos', () => {
         cy.get('#enableAfter').click({timeout: 0, force: true})
     })
 
-    it.only('Click por posicion', ()=>{
+    it('Click por posicion', ()=>{
         cy.visit('/buttons')
         cy.get('button').eq(3).parent().parent().click('topRight')
         cy.get('button').eq(3).parent().parent().click('bottomLeft')
         cy.get('button').eq(3).parent().parent().click(5,60)
     })
 
+    it('Input type text', ()=>{
+        cy.visit('/automation-practice-form')
+        cy.get('#firstName').type('Ivan')
+        cy.get('#lastName').type('Maldonado')
 
+        cy.get('#firstName').type('{selectAll}{backspace}')
+        cy.get('#firstName').type('Otro nombre')
+        cy.get('#firstName').clear()
+    })
+
+    it.only('Checkboxes y radio buttons', ()=>{
+        cy.visit('/automation-practice-form')
+        // cy.get('#gender-radio-1').click()
+        // cy.get('#gender-radio-1').click({force:true})
+        // cy.get('#gender-radio-1').check({force:true})
+        cy.get('label[for="gender-radio-1"]').click()
+
+        // cy.get('#hobbies-checkbox-1').click({force:true})
+        // cy.get('#hobbies-checkbox-1').check({force:true})
+        // cy.get('#hobbies-checkbox-1').uncheck({force:true})
+        cy.get('label[for="hobbies-checkbox-1"]').click()
+        cy.get('label[for="hobbies-checkbox-1"]').click()
+    })
 
 })
